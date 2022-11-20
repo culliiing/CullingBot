8246b75298ecdd7693d5a288d29cc5776fe2e94d
Blissling <unconfigured@null.spigotmc.org>
2022-11-20 12:33:05 +0100

    Add project files.

---
+++ CullingBot/Program.cs
@@ -0,0 +1,37 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient client;
+
+    public async Task MainAsync()
+    {
+        client = new DiscordSocketClient();
+
+        client.Log += Log;
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = "token";
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
